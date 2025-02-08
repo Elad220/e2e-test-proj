3d9041dc499ea2ffd012089f529bcb5288d999c0
Elad Asaf <Elad220@gmail.com>
2025-02-08 19:08:32 +0200

    Init project
    Use uv init to initialize project and add a main.py which creates a FastAPI
    app with a default route and /myroute.
    
    Signed-off-by: Elad Asaf <Elad220@gmail.com>

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def read_root():
+    return "Homepage"
+
+
+@app.get("/myroute", response_class=PlainTextResponse)
+async def my_route():
+    return "Hello, World!"
+
+
+# Run the application:
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
